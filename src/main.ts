701283c30cf93a4b832d3925ae26a6adf23b6650
aubert-co <blacket021@gmail.com>
2026-08-02 12:38:26 -0300

    feat: add ValidationPipe to main bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import { HttpExceptionFilter } from './commom/filters/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist:true,
+      transform:true
+    })
+  )
   app.useGlobalFilters(app.get(HttpExceptionFilter));
   await app.listen(process.env.PORT ?? 3000);
 }

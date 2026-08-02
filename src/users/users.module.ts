025e7747ec7bb6b9805da556e65e45ff1b392745
aubert-co <blacket021@gmail.com>
2026-08-02 12:34:29 -0300

    feat: add Mongoose module to users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './controller/users.controller';
 import { UsersService } from './services/users.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema, User } from './schemas/user.schema';
+import { UsersRepository } from './repository/users.repository';
 @Module({
+  imports:[
+     MongooseModule.forFeature([
+          {
+            schema: UserSchema,
+            name: User.name,
+          },
+        ]),
+  ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService,UsersRepository],
+  exports:[
+    UsersService,
+    UsersRepository
+  ]
 })
 export class UsersModule {}

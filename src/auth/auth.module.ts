69434a74ac024378f412739bf2a8e859643fdb49
aubert-co <blacket021@gmail.com>
2026-08-05 22:28:51 -0300

    feat(backend): add JWT authentication infrastructure

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,23 +3,34 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './services/auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import {  ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+
 import { JwtTokenService } from './services/jwt.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [UsersModule,
-  JwtModule.registerAsync({
-        global: true,
-        inject: [ConfigService],
-        useFactory: (config: ConfigService) => ({
-          secret: config.get<string>('JWT_SECRET'),
-          signOptions: {
-            expiresIn: '7d',
-          },
-        }),
-      })
-    ],
-  controllers: [AuthController],
-  providers: [AuthService, JwtTokenService],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      global: true,
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '7d',
+        },
+      }),
+    }),
+  ],
+  controllers: [
+    AuthController,
+  ],
+  providers: [
+    AuthService,
+    JwtTokenService,
+    JwtStrategy,
+  ],
 })
 export class AuthModule {}
